2ca56aaacd7aa282430d0b3b7fcddb3f6dff5af1
Andrew <merrillman32@gmail.com>
2020-07-03 09:38:34 -0600

    updated 1up and added lets split

--- keyboards/1upkeyboards/1up60hse/keymaps/ALMerrill/keymap.c
+++ keyboards/1upkeyboards/1up60hse/keymaps/ALMerrill/keymap.c
@@ -23,19 +23,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------------------------------------+
    * |  Tab   |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |    \   |
    * |-----------------------------------------------------------------------------------------+
-   * |   L1    |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
+   * |   EscC    |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
    * |-----------------------------------------------------------------------------------------+
    * |   Shift   |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |      Shift      | Tap Shift - Up Arrow
    * |-----------------------------------------------------------------------------------------+
-   * | Ctrl  |  GUI  |  Alt  |              Space              |  L3   |  GUI  |  L1   | Ctrl  | Tap Gui/L1/Ctrl - Left/Down/Right Arrow
+   * | Ctrl  |  Alt(GUI)  |  Alt  |              Space              |  L3   |  GUI  |  L1   | Ctrl  | Tap Gui/L1/Ctrl - Left/Down/Right Arrow
    * `-----------------------------------------------------------------------------------------'
    */
   [0] = LAYOUT_60_ansi(
     GRAVE_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-    MO(1), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+    LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP),
-    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, MO(3), RGUI_T(KC_LEFT), LT(1, KC_DOWN), RCTL_T(KC_RIGHT)
+    KC_LCTL, LALT_T(KC_LGUI), MO(1), KC_SPC, MO(3), RGUI_T(KC_LEFT), LT(1, KC_DOWN), RCTL_T(KC_RIGHT)
   ),
 
 
