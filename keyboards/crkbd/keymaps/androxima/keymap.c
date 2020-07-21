f5bf5040883f7369b457ff5bc94057aae4e4679a
Andrew <merrillman32@gmail.com>
2020-07-21 15:21:55 -0600

    small gaming change

--- keyboards/crkbd/keymaps/androxima/keymap.c
+++ keyboards/crkbd/keymaps/androxima/keymap.c
@@ -53,8 +53,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_LOL] = LAYOUT(
 		KC_ESC, KC_1, KC_2, KC_3, 	KC_4,	KC_5, 				KC_Y, KC_NO, KC_NO, KC_P, KC_NO, QWERTY, 
 		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, 					KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _TYPE, 
-		KC_NO, KC_A, KC_S, KC_D, KC_F, KC_G, 					KC_B, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
-									LOL_F, CCTL, VALT, 	KC_NO, KC_NO, KC_NO
+		_LSFT, 	KC_A, KC_S, KC_D, KC_F, KC_G, 					KC_B, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
+									KC_LCTL, LOL_F, VALT, 	KC_NO, KC_NO, KC_NO
 	),
 	[_LOL_F] = LAYOUT(
 		KC_NO, KC_6, KC_7, KC_NO, KC_6, KC_7, 				KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB, KC_Q, 	KC_W, 	KC_E, 	KC_R, 	KC_T, 			KC_Y, 	KC_U, 	KC_I, 	KC_O, 	KC_P, 		KC_BSPC, 
 		ESCC, 	KC_A, 	KC_S, 	KC_D, 	KC_F, 	KC_G, 			KC_H, 	KC_J, 	KC_K, 	KC_L, 	KC_SCLN, 	KC_TRNS, 
 		_LSFT, 	KC_Z, 	KC_X, 	KC_C, 	KC_V, 	KC_B, 			KC_N, 	KC_M, 	COMM, 	KC_DOT, KC_SLSH, 	SFTENT, 
-									GUIC, 	KC_NO, SFTENT, 	KC_SPC, _TYPE_2, 	KC_RALT
+									GUIC, 	KC_NO, SFTENT, 	KC_SPC, KC_NO, 	LOL
 	),
 	[_LOL_TYPE_2] = LAYOUT(
 		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 				KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
