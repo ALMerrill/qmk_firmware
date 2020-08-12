c740d09f5271de95550f76d0cbcc6be26d630907
Andrew <merrillman32@gmail.com>
2020-08-12 08:24:35 -0600

    updated 1up to be more similar to corne, modified corne to swith raise and lower layers for better compatibility with space

--- keyboards/crkbd/keymaps/androxima/keymap.c
+++ keyboards/crkbd/keymaps/androxima/keymap.c
@@ -1,12 +1,13 @@
 #include QMK_KEYBOARD_H
 
-enum layer_names { _QWERTY, _DVORAK, _LOWER, _RAISE, _ADJUST, _LOL, _LOL_F, _LOL_TYPE, _LOL_TYPE_2 };
+enum layer_names { _QWERTY, _DVORAK, _LOWER, _RAISE, _ADJUST, _NUMBER, _LOL, _LOL_F, _LOL_TYPE, _LOL_TYPE_2 };
 
 #define QWERTY TO(_QWERTY)
 #define DVORAK TO(_DVORAK)
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 #define ADJUST MO(_ADJUST)
+#define NUMBER MO(_NUMBER)
 
 #define LOENT LT(_LOWER, KC_ENT)
 #define RASPC LT(_RAISE, KC_SPC)
@@ -16,6 +17,7 @@ enum layer_names { _QWERTY, _DVORAK, _LOWER, _RAISE, _ADJUST, _LOL, _LOL_F, _LOL
 #define _LSFT KC_LSFT
 #define COMM KC_COMM
 #define ESCC LCTL_T(KC_ESC)
+#define BSPC_A LALT_T(KC_BSPC)
 
 
 
@@ -27,43 +29,43 @@ enum layer_names { _QWERTY, _DVORAK, _LOWER, _RAISE, _ADJUST, _LOL, _LOL_F, _LOL
 #define CCTL LCTL_T(KC_C)
 #define VALT LALT_T(KC_V)
 
-enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE,
-  ADJUST,
-};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_QWERTY] = LAYOUT(
 		KC_TAB, KC_Q, 	KC_W, 	KC_E, 	KC_R, 	KC_T, 			KC_Y, 	KC_U, 	KC_I, 	KC_O, 	KC_P, 		KC_BSPC, 
 		ESCC, 	KC_A, 	KC_S, 	KC_D, 	KC_F, 	KC_G, 			KC_H, 	KC_J, 	KC_K, 	KC_L, 	KC_SCLN, 	KC_QUOT, 
 		_LSFT, 	KC_Z, 	KC_X, 	KC_C, 	KC_V, 	KC_B, 			KC_N, 	KC_M, 	COMM, 	KC_DOT, KC_SLSH, 	SFTENT, 
-									GUIC, 	LOENT, KC_LSFT, KC_BSPC,  RASPC,  KC_RALT
+									GUIC, 	LOENT, KC_LSFT, BSPC_A,  RASPC,  NUMBER
 	),
 	[_DVORAK] = LAYOUT(
 		KC_TAB, COMM,	DOT, 	KC_SLSH,KC_P, 	KC_Y, 			KC_F, 	KC_G, 	KC_C, 	KC_R, 	KC_L, 		KC_BSPC, 
 		ESCC, 	KC_A, 	KC_O, 	KC_E, 	KC_U, 	KC_I, 			KC_D, 	KC_H, 	KC_T, 	KC_N, 	KC_S, 		KC_QUOT, 
 		_LSFT, 	KC_SCLN,KC_Q, 	KC_J, 	KC_K, 	KC_X, 			KC_B, 	KC_M, 	KC_W, 	KC_V, 	KC_Z, 		SFTENT, 
-									GUIC, 	LOENT, KC_LSFT, KC_BSPC,  RASPC,  KC_RALT
+									GUIC, 	LOENT, KC_LSFT, BSPC_A,  RASPC,  NUMBER
 	),
 	[_LOWER] = LAYOUT(
-		KC_GRV, KC_NO, 	KC_7, 	KC_8, 	KC_9, 	KC_NO, 			KC_HOME, 	KC_PGDN, 	KC_PGUP, 	KC_END, 	KC_PSCR, 	KC_DEL, 
-		KC_LCTL,KC_NO, 	KC_4, 	KC_5, 	KC_6, 	KC_NO, 			KC_LEFT, 	KC_DOWN, 	KC_UP, 		KC_RGHT, 	KC_INS, 	KC_BSLS, 
-		_LSFT, 	KC_0, 	KC_1, 	KC_2, 	KC_3, 	KC_L, 			KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 
-									KC_LGUI,KC_TRNS,  KC_LSFT, KC_BSPC, ADJUST, KC_RALT
-	),
-	[_RAISE] = LAYOUT(
 		KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, 		KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, 
 		KC_LCTL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, 			KC_UNDS, KC_PLUS, KC_EQL, KC_LCBR, KC_RCBR, KC_PIPE, 
 		_LSFT, KC_F6, KC_7, KC_8, KC_9, KC_F10, 				KC_NO, KC_MINS, KC_UNDS, KC_LBRC, KC_RBRC, SFTENT, 
-									KC_LGUI, ADJUST, KC_LSFT, KC_BSPC, KC_TRNS, KC_RALT
+									KC_LGUI, ADJUST, KC_LSFT, BSPC_A, KC_TRNS, NUMBER
+	),
+	[_RAISE] = LAYOUT(
+		KC_GRV, KC_NO, 	KC_7, 	KC_8, 	KC_9, 	KC_NO, 			KC_HOME, 	KC_PGDN, 	KC_PGUP, 	KC_END, 	KC_PSCR, 	KC_DEL, 
+		KC_LCTL,KC_NO, 	KC_4, 	KC_5, 	KC_6, 	KC_NO, 			KC_LEFT, 	KC_DOWN, 	KC_UP, 		KC_RGHT, 	KC_INS, 	KC_BSLS, 
+		_LSFT, 	KC_0, 	KC_1, 	KC_2, 	KC_3, 	KC_L, 			KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 		KC_NO, 
+									KC_LGUI,KC_TRNS,  KC_LSFT, BSPC_A, ADJUST, NUMBER
 	),
 	[_ADJUST] = LAYOUT(
 		RESET, KC_NO, KC_NO, KC_NO, KC_NO, 	KC_NO, 				KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO, LOL, 
 		RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, KC_L, KC_NO,	 	KC_WSCH, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO, QWERTY, 
 		RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, KC_NO, KC_L, 		KC_WBAK, KC_WFWD, KC_BRID, KC_BRIU, KC_NO, DVORAK, 
-									KC_LGUI, KC_TRNS,KC_LSFT, KC_BSPC, KC_TRNS, KC_RALT
+									KC_LGUI, KC_TRNS,KC_LSFT, BSPC_A, KC_TRNS, NUMBER
+	),
+	[_NUMBER] = LAYOUT(
+		KC_TAB, KC_1, 	KC_2, 	KC_3, 	KC_4, 	KC_5, 			KC_6, 	KC_7, 	KC_8, 	KC_9, 	KC_0, 		KC_BSPC, 
+		ESCC, 	KC_A, 	KC_S, 	KC_D, 	KC_F, 	KC_G, 			KC_H, 	KC_J, 	KC_K, 	KC_L, 	KC_SCLN, 	KC_QUOT, 
+		_LSFT, 	KC_Z, 	KC_X, 	KC_C, 	KC_V, 	KC_B, 			KC_N, 	KC_M, 	COMM, 	KC_DOT, KC_SLSH, 	SFTENT, 
+									GUIC, 	LOENT, KC_LSFT, BSPC_A,  RASPC,  KC_TRNS
 	),
 	[_LOL] = LAYOUT(
 		KC_ESC, KC_1, KC_2, KC_3, 	KC_4,	KC_5, 				KC_Y, KC_NO, KC_NO, KC_P, KC_NO, QWERTY, 
