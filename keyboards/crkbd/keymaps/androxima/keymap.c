7d32ac0ef5d40719b1eb9b8dfd070b7fbbb32a80
Andrew <merrillman32@gmail.com>
2020-09-20 16:39:15 -0600

    1up via

--- keyboards/crkbd/keymaps/androxima/keymap.c
+++ keyboards/crkbd/keymaps/androxima/keymap.c
@@ -33,7 +33,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB, KC_Q, 	KC_W, 	KC_E, 	KC_R, 	KC_T, 			KC_Y, 	KC_U, 	KC_I, 	KC_O, 	KC_P, 		KC_BSPC, 
 		ESCC, 	KC_A, 	KC_S, 	KC_D, 	KC_F, 	KC_G, 			KC_H, 	KC_J, 	KC_K, 	KC_L, 	KC_SCLN, 	KC_QUOT, 
 		_LSFT, 	KC_Z, 	KC_X, 	KC_C, 	KC_V, 	KC_B, 			KC_N, 	KC_M, 	COMM, 	KC_DOT, KC_SLSH, 	SFTENT, 
-                                    A
 									KC_LGUI, 	LOWER, KC_LSFT, BSPC_A,  RASPC,  NUMBER
 	),
 	[_DVORAK] = LAYOUT(
