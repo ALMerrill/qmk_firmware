c18aef0dc5583d864d9ef44528a8f17e94155db1
Andrew <merrillman32@gmail.com>
2020-10-24 15:03:24 -0600

    improved corne, added corne-like layout for planck, and bdn9 layout

--- keyboards/crkbd/keymaps/androxima/keymap.c
+++ keyboards/crkbd/keymaps/androxima/keymap.c
@@ -11,6 +11,7 @@ enum layer_names { _QWERTY, _DVORAK, _LOWER, _RAISE, _ADJUST, _NUMBER, _LOL, _LO
 
 #define LOENT LT(_LOWER, KC_ENT)
 #define RASPC LT(_RAISE, KC_SPC)
+#define ADJSPC LT(_ADJUST, KC_SPC)
 #define ESCC LCTL_T(KC_ESC)
 #define SFTENT KC_SFTENT
 #define GUIC LCTL_T(KC_LGUI)
@@ -32,19 +33,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB, KC_Q, 	KC_W, 	KC_E, 	KC_R, 	KC_T, 			KC_Y, 	KC_U, 	KC_I, 	KC_O, 	KC_P, 		KC_BSPC, 
 		ESCC, 	KC_A, 	KC_S, 	KC_D, 	KC_F, 	KC_G, 			KC_H, 	KC_J, 	KC_K, 	KC_L, 	KC_SCLN, 	KC_QUOT, 
 		_LSFT, 	KC_Z, 	KC_X, 	KC_C, 	KC_V, 	KC_B, 			KC_N, 	KC_M, 	COMM, 	KC_DOT, KC_SLSH, 	SFTENT, 
-									KC_LGUI, 	LOWER, KC_LSFT, BSPC_A,  RASPC,  NUMBER
+									KC_LGUI, 	LOWER, SFTENT, BSPC_A,  RASPC,  NUMBER
 	),
 	[_DVORAK] = LAYOUT(
 		KC_TAB, KC_QUOT,KC_COMM,KC_DOT,	KC_P, 	KC_Y, 			KC_F, 	KC_G, 	KC_C, 	KC_R, 	KC_L, 		KC_BSPC, 
 		ESCC, 	KC_A, 	KC_O, 	KC_E, 	KC_U, 	KC_I, 			KC_D, 	KC_H, 	KC_T, 	KC_N, 	KC_S, 		KC_SLSH, 
 		_LSFT, 	KC_SCLN,KC_Q, 	KC_J, 	KC_K, 	KC_X, 			KC_B, 	KC_M, 	KC_W, 	KC_V, 	KC_Z, 		SFTENT, 
-									GUIC, 	LOWER, KC_LSFT, BSPC_A,  RASPC,  NUMBER
+									KC_LGUI, 	LOWER, SFTENT, BSPC_A,  RASPC,  NUMBER
 	),
 	[_LOWER] = LAYOUT(
 		KC_TILD, KC_NO, KC_7, 	KC_8, 	KC_9, 	KC_0, 		    KC_CIRC,    KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, 
 		KC_LCTL, KC_NO, KC_4, 	KC_5, 	KC_6, 	KC_NO, 			KC_NO,    KC_PLUS, KC_EQL, KC_LCBR, KC_RCBR, KC_PIPE, 
 		_LSFT, 	 KC_0, 	KC_1, 	KC_2, 	KC_3, 	KC_L, 			KC_NO,      KC_MINS, KC_UNDS, KC_LBRC, KC_RBRC, SFTENT, 
-									KC_LGUI, KC_TRNS, KC_LSFT, BSPC_A, ADJUST, NUMBER
+									KC_LGUI, KC_TRNS, KC_LSFT, BSPC_A, ADJSPC, NUMBER
 	),
 	[_RAISE] = LAYOUT(
 		KC_GRV,  KC_EXLM, KC_AT, 	KC_HASH, KC_DLR, KC_PERC, 		KC_CIRC, 	KC_AMPR, 	KC_ASTR, 	KC_LPRN, 	KC_RPRN, 	KC_DEL, 
@@ -60,8 +61,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	),
 	[_NUMBER] = LAYOUT(
 		KC_TAB, KC_1, 	KC_2, 	KC_3, 	KC_4, 	KC_5, 			KC_6, 	KC_7, 	KC_8, 	KC_9, 	KC_0, 		KC_BSPC, 
-		ESCC, 	KC_F1, 	KC_F2, 	KC_F3, 	KC_F4, 	KC_F5, 			KC_F6, 	KC_NO, 	KC_NO, 	KC_NO, 	KC_NO, 		KC_QUOT, 
-		_LSFT, 	KC_F7, 	KC_F8, 	KC_F9, 	KC_F10, KC_F11, 		KC_F12, KC_NO, 	KC_NO, 	KC_NO, 	KC_NO, 		SFTENT, 
+		ESCC, 	KC_A, 	KC_S, 	KC_D, 	KC_F, 	KC_G, 			KC_H, 	KC_J,   KC_K, 	KC_L, 	KC_NO, 		KC_QUOT, 
+		_LSFT, 	KC_Z, 	KC_X, 	KC_C, 	KC_V,   KC_B, 	    	KC_N,   KC_M,   COMM, 	KC_DOT, KC_SLSH,	SFTENT, 
 									GUIC, 	LOWER, KC_LSFT, BSPC_A,  RASPC,  KC_TRNS
 	),
 	[_LOL] = LAYOUT(
