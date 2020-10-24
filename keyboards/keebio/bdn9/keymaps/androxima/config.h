345e49a49675c6dec5045592ecb65b969704f775
Andrew <merrillman32@gmail.com>
2020-10-24 15:03:24 -0600

    improved corne, added corne-like layout for planck, and bdn9 layout

---
+++ keyboards/keebio/bdn9/keymaps/androxima/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef RGB_MATRIX_ENABLE
+#    define RGB_MATRIX_KEYPRESSES
+#endif
+
+#define TAP_CODE_DELAY 10
+
+#define ENCODERS_PAD_A { D1, F5 }
+#define ENCODERS_PAD_B { D0, F6 }
+
