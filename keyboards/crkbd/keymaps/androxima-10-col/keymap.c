4a91073f119ad0dd36e25f0655c97ad8592393fa
Andrew Merrill <andrew.merrill@deliciousai.com>
2021-02-13 21:34:19 -0700

    added home row mods

--- keyboards/crkbd/keymaps/androxima-10-col/keymap.c
+++ keyboards/crkbd/keymaps/androxima-10-col/keymap.c
@@ -12,6 +12,8 @@ enum layer_names { _QWERTY, _DVORAK, _LOWER, _RAISE, _ADJUST, _NUMBER, _LOL, _LO
 #define LOENT LT(_LOWER, KC_ENT)
 #define RASPC LT(_RAISE, KC_SPC)
 #define ADJSPC LT(_ADJUST, KC_SPC)
+#define NUMESC LT(_NUMBER, KC_ESC)
+#define LOWENT LT(_LOWER, KC_ENT)
 #define ESCC LCTL_T(KC_ESC)
 #define SFTENT KC_SFTENT
 #define GUIC LCTL_T(KC_LGUI)
@@ -20,6 +22,17 @@ enum layer_names { _QWERTY, _DVORAK, _LOWER, _RAISE, _ADJUST, _NUMBER, _LOL, _LO
 #define ESCC LCTL_T(KC_ESC)
 #define BSPC_A LALT_T(KC_BSPC)
 
+// Left-hand home row mods
+#define CTL_A LCTL_T(KC_A)
+#define ALT_S LALT_T(KC_S)
+#define GUI_D LGUI_T(KC_D)
+#define SFT_F LSFT_T(KC_F)
+
+// Right-hand home row mods
+#define SFT_J RSFT_T(KC_J)
+#define GUI_K RGUI_T(KC_K)
+#define ALT_L LALT_T(KC_L)
+#define CTL_SCLN RCTL_T(KC_SCLN)
 
 #define LOL TO(_LOL)
 #define LOL_F LT(_LOL_F,KC_SPC)
@@ -27,12 +40,13 @@ enum layer_names { _QWERTY, _DVORAK, _LOWER, _RAISE, _ADJUST, _NUMBER, _LOL, _LO
 #define CCTL LCTL_T(KC_C)
 #define VALT LALT_T(KC_V)
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_QWERTY] = LAYOUT(
-		KC_NO,  KC_Q, 	KC_W, 	KC_E, 	KC_R, 	KC_T, 			KC_Y, 	KC_U, 	KC_I, 	KC_O, 	KC_P, 		KC_BSPC, 
-		ESCC, 	KC_A, 	KC_S, 	KC_D, 	KC_F, 	KC_G, 			KC_H, 	KC_J, 	KC_K, 	KC_L, 	KC_SCLN, 	KC_QUOT, 
-		_LSFT, 	KC_Z, 	KC_X, 	KC_C, 	KC_V, 	KC_B, 			KC_N, 	KC_M, 	COMM, 	KC_DOT, KC_SLSH, 	SFTENT,
-									KC_LGUI, 	LOWER, SFTENT, BSPC_A,  RASPC,  NUMBER
+		KC_NO,  KC_Q, 	KC_W, 	KC_E, 	KC_R, 	KC_T, 			KC_Y, 	KC_U, 	KC_I, 	KC_O, 	KC_P, 		KC_NO, 
+		KC_NO, 	CTL_A,  ALT_S,  GUI_D,  SFT_F,  KC_G,           KC_H,   SFT_J,  GUI_K,  ALT_L,  CTL_SCLN, 	KC_NO, 
+		KC_NO, 	KC_Z, 	KC_X, 	KC_C, 	KC_V, 	KC_B, 			KC_N, 	KC_M, 	COMM, 	KC_DOT, KC_SLSH, 	KC_NO,
+									KC_LGUI,  LOWENT, SFTENT, KC_BSPC,  RASPC,  NUMESC
 	),
 	[_DVORAK] = LAYOUT(
 		KC_TAB, KC_QUOT,KC_COMM,KC_DOT,	KC_P, 	KC_Y, 			KC_F, 	KC_G, 	KC_C, 	KC_R, 	KC_L, 		KC_BSPC, 
@@ -50,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GRV,  KC_EXLM, KC_AT, 	KC_HASH, KC_DLR, KC_PERC, 		KC_CIRC, 	KC_AMPR, 	KC_ASTR, 	KC_LPRN, 	KC_RPRN, 	KC_DEL, 
 		KC_LCTL, KC_TAB, KC_PGDN, 	KC_PGUP, KC_END, KC_NO, 		KC_LEFT, 	KC_DOWN, 	KC_UP, 		KC_RGHT, 	KC_QUOT, 	KC_BSLS,
 		_LSFT, 	 KC_NO,   KC_NO, 	KC_NO, 	KC_NO, 	KC_L, 			KC_HOME, 	KC_PGDN, 	KC_PGUP, 	KC_END, 	KC_NO, 		KC_NO, 
-									KC_LCTRL,LOWER,  KC_LSFT,    BSPC_A, KC_TRNS, NUMBER
+									KC_LCTRL,LOWER,  KC_LALT,    BSPC_A, KC_TRNS, NUMBER
 	),
 	[_ADJUST] = LAYOUT(
 		RESET, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 		            KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO, LOL, 
