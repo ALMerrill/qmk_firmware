64b65032129e035215eed0ca9a53b2694380b11c
Andrew Merrill <andrew.merrill@deliciousai.com>
2021-02-24 17:57:30 -0700

    added planck androxima, got it working

--- keyboards/crkbd/keymaps/androxima-10-col/keymap.c
+++ keyboards/crkbd/keymaps/androxima-10-col/keymap.c
@@ -1,8 +1,9 @@
 #include QMK_KEYBOARD_H
 
-enum layer_names { _QWERTY, _DVORAK, _LOWER, _RAISE, _ADJUST, _NUMBER, _LOL, _LOL_F};
+enum layer_names { _QWERTY, _HOME, _DVORAK, _LOWER, _RAISE, _ADJUST, _NUMBER, _LOL, _LOL_F};
 
 #define QWERTY TO(_QWERTY)
+#define HOME TO(_HOME)
 #define DVORAK TO(_DVORAK)
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
@@ -17,6 +18,8 @@ enum layer_names { _QWERTY, _DVORAK, _LOWER, _RAISE, _ADJUST, _NUMBER, _LOL, _LO
 #define ESCC LCTL_T(KC_ESC)
 #define SFTENT KC_SFTENT
 #define GUIC LCTL_T(KC_LGUI)
+#define GUITAB LGUI_T(KC_TAB)
+#define SFT_TAB LSFT_T(KC_TAB)
 #define _LSFT KC_LSFT
 #define COMM KC_COMM
 #define ESCC LCTL_T(KC_ESC)
@@ -43,10 +46,16 @@ enum layer_names { _QWERTY, _DVORAK, _LOWER, _RAISE, _ADJUST, _NUMBER, _LOL, _LO
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_QWERTY] = LAYOUT(
+		KC_TAB, KC_Q, 	KC_W, 	KC_E, 	KC_R, 	KC_T, 			KC_Y, 	KC_U, 	KC_I, 	KC_O, 	KC_P, 		KC_BSPC, 
+		ESCC, 	KC_A, 	KC_S, 	KC_D, 	KC_F, 	KC_G, 			KC_H, 	KC_J, 	KC_K, 	KC_L, 	KC_SCLN, 	KC_QUOT, 
+		_LSFT, 	KC_Z, 	KC_X, 	KC_C, 	KC_V, 	KC_B, 			KC_N, 	KC_M, 	COMM, 	KC_DOT, KC_SLSH, 	SFTENT, 
+								   SFT_TAB,  LOWENT, KC_LGUI, BSPC_A,  RASPC,  NUMESC
+	),
+	[_HOME] = LAYOUT(
 		KC_NO,  KC_Q, 	KC_W, 	KC_E, 	KC_R, 	KC_T, 			KC_Y, 	KC_U, 	KC_I, 	KC_O, 	KC_P, 		KC_NO, 
 		KC_NO, 	CTL_A,  ALT_S,  GUI_D,  SFT_F,  KC_G,           KC_H,   SFT_J,  GUI_K,  ALT_L,  CTL_SCLN, 	KC_NO, 
 		KC_NO, 	KC_Z, 	KC_X, 	KC_C, 	KC_V, 	KC_B, 			KC_N, 	KC_M, 	COMM, 	KC_DOT, KC_SLSH, 	KC_NO,
-									KC_LGUI,  LOWENT, SFTENT, KC_BSPC,  RASPC,  NUMESC
+									SFT_TAB,  LOWENT, KC_LGUI, KC_BSPC,  RASPC,  NUMESC
 	),
 	[_DVORAK] = LAYOUT(
 		KC_TAB, KC_QUOT,KC_COMM,KC_DOT,	KC_P, 	KC_Y, 			KC_F, 	KC_G, 	KC_C, 	KC_R, 	KC_L, 		KC_BSPC, 
@@ -56,19 +65,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	),
 	[_LOWER] = LAYOUT(
 		KC_TILD, KC_NO, KC_7, 	KC_8, 	KC_9, 	KC_0, 		    KC_CIRC,    KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, 
-		KC_NO, KC_NO, KC_4, 	KC_5, 	KC_6, 	KC_NO, 			KC_NO,    KC_PLUS, KC_EQL, KC_LCBR, KC_RCBR, KC_PIPE, 
+		KC_NO, KC_NO, KC_4, 	KC_5, 	KC_6, 	KC_ENT, 			KC_BSPC,    KC_PLUS, KC_EQL, KC_LCBR, KC_RCBR, KC_PIPE, 
 		KC_NO, 	 KC_0, 	KC_1, 	KC_2, 	KC_3, 	KC_NO, 			KC_NO,      KC_MINS, KC_UNDS, KC_LBRC, KC_RBRC, SFTENT, 
-									KC_LGUI, KC_TRNS, KC_LSFT, BSPC_A, ADJSPC, NUMBER
+									KC_LGUI, KC_TRNS, KC_LSFT, KC_DEL, ADJSPC, NUMBER
 	),
 	[_RAISE] = LAYOUT(
 		KC_GRV,  KC_EXLM, KC_AT, 	KC_HASH, KC_DLR, KC_PERC, 		KC_CIRC, 	KC_AMPR, 	KC_ASTR, 	KC_LPRN, 	KC_RPRN, 	KC_DEL, 
 		KC_LCTL, KC_TAB, KC_PGDN, 	KC_PGUP, KC_END, KC_NO, 		KC_LEFT, 	KC_DOWN, 	KC_UP, 		KC_RGHT, 	KC_QUOT, 	KC_BSLS,
 		_LSFT, 	 KC_NO,   KC_NO, 	KC_NO, 	KC_NO, 	KC_L, 			KC_HOME, 	KC_PGDN, 	KC_PGUP, 	KC_END, 	KC_NO, 		KC_NO, 
-									KC_LCTRL,LOWER,  KC_LALT,    BSPC_A, KC_TRNS, NUMBER
+									KC_LGUI,LOWER,  KC_LALT,   KC_NO, KC_TRNS, NUMBER
 	),
 	[_ADJUST] = LAYOUT(
 		RESET, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 		            KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, KC_NO, LOL, 
-		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,	 	            KC_MUTE, KC_VOLD, KC_VOLU, KC_NO, KC_NO, QWERTY, 
+		KC_NO, KC_NO, KC_NO, KC_NO, HOME, KC_NO,	 	            KC_MUTE, KC_VOLD, KC_VOLU, KC_NO, KC_NO, QWERTY, 
 		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 		            KC_NO, KC_BRID, KC_BRIU, KC_NO, KC_NO, DVORAK, 
 								KC_LGUI, KC_TRNS,KC_LSFT,       BSPC_A, KC_TRNS, NUMBER
 	),
--- keyboards/planck/keymaps/androxima/rules.mk
+++ keyboards/planck/keymaps/androxima/rules.mk
@@ -1,1 +1,3 @@
 SRC += muse.c
+
+EXTRAKEY_ENABLE = yes
